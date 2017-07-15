ecf4b6dbadcc75a031c3efc61e3e36e9ff72ea06
Steven Mertens <steven.mertens@catay.be>
2017-07-15 16:33:19 +0200

    Set default hosts file definition for a Puppet Server.

--- bootstrap/server/manifests/site.pp
+++ bootstrap/server/manifests/site.pp
@@ -1,8 +1,10 @@
 
-# Set puppet fqdn and alias in hosts file
+# Set FQDN and short host name in hosts file.
+# For a Puppet Server we also set 'puppet' as a host alias.
 
-host {'puppet.example.net': 
+host {$facts['networking']['fqdn']: 
   ensure        => present,
-  host_aliases  => 'puppet',
+  host_aliases  => ["${facts['networking']['hostname']}", "puppet" ]
   ip            => $facts['networking']['interfaces']['eth1']['ip']
 }
+
